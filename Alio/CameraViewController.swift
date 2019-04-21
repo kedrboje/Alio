8abaea87dc92dee111a636f99d3f758556380331
kedrboje <fmrubchenkov@edu.hse.ru>
2019-04-21 21:42:52 +0300

    added two classes for the tables

---
+++ Alio/CameraViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewController.swift
+//  Alio
+//
+//  Created by Федор on 21/04/2019.
+//  Copyright © 2019 Федор. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("Camera Loaded!")
+        super.viewDidLoad()
+    }
+    
+}
