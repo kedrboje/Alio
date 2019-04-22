de76934346c56a213629ae31b3f9b6c4db03894e
kedrboje <fmrubchenkov@edu.hse.ru>
2019-04-22 14:15:52 +0300

    added UIImagePickerController instance and delegate

--- Alio/CameraViewController.swift
+++ Alio/CameraViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet var imageView: UIImageView!
     @IBAction func takePicture(_ sender: UIButton) {
         
+        let imagePicker = UIImagePickerController()
+        // If the device has a camera, take a picture; otherwise,
+        // just pick from photo library
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
