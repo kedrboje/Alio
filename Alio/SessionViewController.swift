b39029e15bae8b2b266b3f7fa3113f757b080b23
kedrboje <fmrubchenkov@edu.hse.ru>
2019-04-23 23:14:50 +0300

    created statr capture session method

--- Alio/SessionViewController.swift
+++ Alio/SessionViewController.swift
@@ -15,8 +15,10 @@ class SessionViewController: UIViewController {
     
     @IBOutlet var imageViewSession: UIImageView!
     
-
     @IBAction func startSessionButton(_ sender: UIButton) {
+        
+        startSession()
+        
     }
     
     @IBOutlet var predictionLabelSession: UILabel!
@@ -26,5 +28,37 @@ class SessionViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    private func startSession() {
+        
+        let session = AVCaptureSession()
+        
+//      Configuration for the captureSession (between begin and commitConfiguration)
+        session.beginConfiguration()
+        let videoSource = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+        
+//      handle availability of videoSource
+        guard let videoSourceInput = try? AVCaptureDeviceInput(device: videoSource!), session.canAddInput(videoSourceInput) else {
+            return
+        }
+        
+//      add input
+        session.addInput(videoSourceInput)
+        let videoSourceOutput = AVCaptureVideoDataOutput()
+        guard session.canAddOutput(videoSourceOutput) else {
+            return
+        }
+        
+//      add output
+        session.addOutput(videoSourceOutput)
+        session.commitConfiguration()
+        
+//      create and add custom layer to the imageViewSession
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame.size = imageViewSession.frame.size
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        imageViewSession.layer.addSublayer(previewLayer)
+    
+        session.startRunning()
+    }
     
 }
